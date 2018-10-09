7d632c7e250c24543c6390e86b8034de63bccaf7
agustin <ablanco3500@hotmail.com>
2018-10-09 19:47:42 -0300

    boludeces

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -1,8 +1,10 @@
 package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -11,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.mygdx.game.GameConstants;
 import com.mygdx.game.Principal;
+import com.mygdx.game.actors.Personaje;
 
 /**
  * Created by Dewa on 2/19/2017.
@@ -20,13 +23,16 @@ public class GameScreen implements Screen {
     final Principal game;
     private Stage stage;
     private Skin mySkin;
+    private Personaje pers,pers2;
+    Texture personaje, personaje2;
 
     public GameScreen(final Principal game){
         this.game = game;
         stage = new Stage(game.screenPort);
         mySkin = new Skin(Gdx.files.internal(GameConstants.skin));
         Gdx.input.setInputProcessor(stage);
-
+        personaje = new Texture("Play.png");
+        personaje2 = new Texture("descargar.jpeg");
         Button homeBtn = new TextButton("HOME",mySkin,"small");
         homeBtn.setSize(GameConstants.col_width,GameConstants.row_height);
         homeBtn.setPosition(0,GameConstants.screenHeight - homeBtn.getHeight());
@@ -49,16 +55,25 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-
+    	
+    	
+    	stage.setDebugAll(true);
+    	pers = new Personaje(personaje,1);
+    	pers2 = new Personaje(personaje2,2);
+    	stage.addActor(pers);
+    	stage.addActor(pers2);
+    	pers.setPosition(500, 10);
+    	pers2.setPosition(0, 10);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
         stage.act();
         stage.draw();
-
+        
     }
 
     @Override
